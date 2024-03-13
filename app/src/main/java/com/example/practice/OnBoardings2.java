b0877e6fc4c1ecf0786319082930aafb5e40c594
rmp <|"/>
2024-03-13 12:04:55 +0300

    ADD new activities

--- app/src/main/java/com/example/practice/OnBoardings2.java
+++ app/src/main/java/com/example/practice/OnBoardings2.java
@@ -6,20 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-
-
-public class OnboardingActivity extends AppCompatActivity {
+public class OnBoardings2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.onboard1);
+        setContentView(R.layout.onboard2);
     }
 
     public void next(View view) {
-        Intent intent = new Intent(this, OnBoardings2.class);
+        Intent intent = new Intent(this, OnBoarding3.class);
         startActivity(intent);
     }
+
     public void skip(View view) {
         Intent intent = new Intent(this, OnBoarding3.class);
         startActivity(intent);
